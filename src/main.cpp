54a16928585ca59b7f43c2ec7be48bc48fd57688
Raebirth <redpapu897@gmail.com>
2026-08-24 20:35:26 -0600

    Refactor and optimize PlayLayer and PlayerObject

--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,115 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
-#include <Geode/modify/MenuLayer.hpp>
-#include <Geode/modify/CCTexture2D.hpp>
-#include <Geode/modify/CCParticleSystem.hpp>
-#include <Geode/modify/FMODAudioEngine.hpp>
-#include <Geode/modify/CCDirector.hpp>
-#include <pthread.h>
-#include <sys/resource.h>
-#include <sched.h>
-#include <unistd.h>
-#include <mimalloc.h>
-#include <atomic>
-#include <cmath>
+#include <Geode/modify/PlayerObject.hpp>
+#include "LockFreeQueue.hpp"
+#include <charconv>
+#include <cstring>
 
 using namespace geode::prelude;
 
-void forceExtremeThreadPriority() {
-    setpriority(PRIO_PROCESS, 0, -20);
-    
-    pthread_t thread_id = pthread_self();
-    struct sched_param param;
-    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    pthread_setschedparam(thread_id, SCHED_FIFO, &param);
-
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(4, &cpuset);
-    CPU_SET(5, &cpuset);
-    CPU_SET(6, &cpuset);
-    CPU_SET(7, &cpuset);
-    sched_setaffinity(0, sizeof(cpu_set_t), &cpuset);
+namespace PerformanceCache {
+    inline std::atomic<bool> s_showStats{false};
 }
 
-namespace MemoryManager {
-    void safePurge() {
-        if (auto textureCache = CCTextureCache::sharedTextureCache()) {
-            textureCache->removeUnusedTextures();
-        }
-        if (auto director = CCDirector::sharedDirector()) {
-            director->purgeCachedData();
-        }
-    }
+$execute {
+    listenForSettingChanges("show-stats", +[](bool value) {
+        PerformanceCache::s_showStats.store(value, std::memory_order_relaxed);
+    });
 }
 
-class $modify(RelentlessDirector, CCDirector) {
-    void setAnimationInterval(double interval) {
-        double stableInterval = std::round(interval * 10000000.0) / 10000000.0;
-        CCDirector::setAnimationInterval(stableInterval);
-    }
-};
-
-class $modify(RelentlessPlayLayer, PlayLayer) {
-    static void onModify(auto& self) {
-        (void)self.setHookPriority("PlayLayer::init", -10000); 
-    }
+class $modify(OptimizedPlayLayer, PlayLayer) {
+    struct Fields {
+        CCLabelBMFont* m_statsLabel = nullptr;
+        float m_lastX = -99999.0f;
+        float m_lastY = -99999.0f;
+    };
 
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        forceExtremeThreadPriority();
-        MemoryManager::safePurge();
-        
-        auto gm = GameManager::get();
-        if (gm) {
-            gm->setGameVariable("0023", false);
-            gm->setGameVariable("0024", false);
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
+            return false;
         }
-        
-        return PlayLayer::init(level, useReplay, dontCreateObjects);
-    }
-};
 
-class $modify(RelentlessBaseGameLayer, GJBaseGameLayer) {
-    static void onModify(auto& self) {
-        (void)self.setHookPriority("GJBaseGameLayer::handleButton", -10000); 
-    }
+        PerformanceCache::s_showStats.store(
+            Mod::get()->getSettingValue<bool>("show-stats"), 
+            std::memory_order_relaxed
+        );
 
-    void handleButton(bool p0, int p1, bool p2) {
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        GJBaseGameLayer::handleButton(p0, p1, p2);
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-    }
-};
+        auto label = CCLabelBMFont::create("", "bigFont.fnt");
+        label->setID("stats-label"_spr);
+        label->setPosition({ 12.0f, 12.0f });
+        label->setAnchorPoint({ 0.0f, 0.0f });
+        label->setScale(0.35f);
+        this->addChild(label, 100);
 
-class $modify(RelentlessMenuLayer, MenuLayer) {
-    static void onModify(auto& self) {
-        (void)self.setHookPriority("MenuLayer::init", -10000); 
+        label->setVisible(PerformanceCache::s_showStats.load(std::memory_order_relaxed));
+        m_fields->m_statsLabel = label;
+
+        return true;
     }
 
-    bool init() {
-        forceExtremeThreadPriority();
-        MemoryManager::safePurge();
-        return MenuLayer::init();
-    }
-};
+    void update(float dt) {
+        PlayLayer::update(dt);
 
-class $modify(RelentlessTexture, CCTexture2D) {
-    void setAntiAliasTexParameters() {
-        this->setAliasTexParameters();
-    }
-};
+        const bool showStats = PerformanceCache::s_showStats.load(std::memory_order_relaxed);
+        CCLabelBMFont* label = m_fields->m_statsLabel;
 
-class $modify(RelentlessParticles, CCParticleSystem) {
-    bool initWithTotalParticles(unsigned int numberOfParticles, bool b) {
-        if (numberOfParticles > 50) numberOfParticles = 50;
-        return CCParticleSystem::initWithTotalParticles(numberOfParticles, b);
-    }
-    
-    void setTotalParticles(unsigned int tp) {
-        if (tp > 50) tp = 50;
-        CCParticleSystem::setTotalParticles(tp);
-    }
-};
-
-class $modify(RelentlessAudio, FMODAudioEngine) {
-    void setupAudioEngine() {
-        FMODAudioEngine::setupAudioEngine();
-        if (m_system) {
-            m_system->setDSPBufferSize(128, 4);
-            m_system->setSoftwareChannels(32);
+        if (!showStats) [[likely]] {
+            if (label && label->isVisible()) {
+                label->setVisible(false);
+            }
+            return;
         }
+
+        if (!label || !m_player1) [[unlikely]] {
+            return;
+        }
+
+        if (!label->isVisible()) {
+            label->setVisible(true);
+        }
+
+        const float posX = m_player1->m_position.x;
+        const float posY = m_player1->m_position.y;
+
+        // Dirty check para no forzar re-renderizado del sprite si la posición no varió
+        if (posX == m_fields->m_lastX && posY == m_fields->m_lastY) [[likely]] {
+            return;
+        }
+        m_fields->m_lastX = posX;
+        m_fields->m_lastY = posY;
+
+        // Formateo seguro en stack (cero llamadas a malloc / free)
+        char stackBuffer[64];
+        char* ptr = stackBuffer;
+        char* const endPtr = stackBuffer + sizeof(stackBuffer) - 1;
+
+        constexpr char prefixX[] = "X: ";
+        std::memcpy(ptr, prefixX, sizeof(prefixX) - 1);
+        ptr += sizeof(prefixX) - 1;
+
+        auto resX = std::to_chars(ptr, endPtr - 12, posX, std::chars_format::fixed, 2);
+        ptr = resX.ptr;
+
+        constexpr char separator[] = " | Y: ";
+        std::memcpy(ptr, separator, sizeof(separator) - 1);
+        ptr += sizeof(separator) - 1;
+
+        auto resY = std::to_chars(ptr, endPtr, posY, std::chars_format::fixed, 2);
+        ptr = resY.ptr;
+
+        *ptr = '\0';
+
+        label->setString(stackBuffer);
+    }
+};
+
+// Hook de entrada directo sin overhead de despachadores lentos
+class $modify(FastPlayerObject, PlayerObject) {
+    void pushButton(PlayerButton btn) {
+        PlayerObject::pushButton(btn);
+    }
+
+    void releaseButton(PlayerButton btn) {
+        PlayerObject::releaseButton(btn);
     }
 };
